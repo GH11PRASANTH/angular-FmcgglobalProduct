5d04d1eda592731dce3e5eca8f15da54e3c6ca5c
GH11PRASANTH <prasanthcse1620@gmail.com>
2022-02-16 18:06:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-hfddan
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-hfddan)
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+
+export interface Product
+ {
+   productId: number;
+    productName: string;
+    price: number;
+    availableQuantity: number;
+    expiryDate:Date;
+  }
+
+export const productsFromFile = [
+    {
+        "productId": "P001",
+        "productName": "fan",
+        "price": 1200.0,
+        "availableQuantity": 4,
+        "expiryDate": "2002-06-25"
+    },
+    {
+        "productId": "P002",
+        "productName": "Heater",
+        "price": 1500.0,
+        "availableQuantity": 2,
+        "expiryDate": "2034-03-12"
+    }
+]
