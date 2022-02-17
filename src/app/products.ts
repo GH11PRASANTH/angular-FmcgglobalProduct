8195607b99903f75e1d91bc4fef1fb7cb341ec7d
GH11PRASANTH <prasanthcse1620@gmail.com>
2022-02-17 09:48:51 +0530

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -11,21 +11,21 @@ export interface Product
 export const productsFromFile = [
     {
         productId: 'P001',
-        productName: 'fan',
+        productName: '1:fan',
         price: 1200.0,
         availableQuantity: 4,
         expiryDate: '2002-06-25'
     },
     {
         productId : 'P002',
-        productName : 'Heater',
+        productName : '2:Heater',
         price: 1500.0,
         availableQuantity : 2,
         expiryDate : '2034-03-12'
     },
     {
         productId : 'P003',
-        productName : 'Mobile',
+        productName : '3:Mobile',
         price: 5000.0,
         availableQuantity : 2,
         expiryDate : '2021-03-12'
